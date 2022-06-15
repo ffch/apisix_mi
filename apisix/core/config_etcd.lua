53f123ec05acb9b684b26cbc5ebc22ebb8d8c861
chenfufei <chenfufei@xiaomi.com>
2022-06-15 16:21:13 +0800

    打包测试

--- apisix/core/config_etcd.lua
+++ apisix/core/config_etcd.lua
@@ -100,13 +100,13 @@ local function readdir(etcd_cli, key, formatter)
     if not etcd_cli then
         return nil, "not inited"
     end
-    print("readdir  key", key)
+    log.info("readdir  key", key)
     local res, err = etcd_cli:readdir(key)
     if not res then
         -- log.error("failed to get key from etcd: ", err)
         return nil, err
     end
-    print("readdir  res", res)
+    log.info("readdir  res", json.encode(res))
     if type(res.body) ~= "table" then
         return nil, "failed to read etcd dir"
     end
